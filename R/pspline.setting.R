75b8145d7e2b079427d970328b59f430e68d2fe4
cli9 <cli9@ncsu.edu>
2016-12-26 00:05:07 -0500

    Update pspline.setting.R
    for equally-spaced knots, difference penalty is used; for knots placement based on quantile, derivative based penalty is adopted

--- R/pspline.setting.R
+++ R/pspline.setting.R
@@ -1,55 +1,30 @@
 pspline.setting <- function(x,knots=select.knots(x,35),
-                            p=3,m=2,periodicity=FALSE,weight=NULL,type="full"){
+                            p=3,m=2,weight=NULL,type="full",
+                            knots.option="equally-spaced"){
   
 # x: the marginal data points
 # knots: the list of interior knots or the numbers of interior knots
-# k: dimension reduction
 # p: degrees for B-splines, with defaults values 3
 # m: orders of difference penalty, with default values 2
+# knots.option: type of knots placement, with default values "equally-spaced"
+
 #require(splines)
 #require(Matrix)
 
 ### design matrix 
 K = length(knots)-2*p-1
 B = spline.des(knots=knots, x=x, ord = p+1,outer.ok = TRUE,sparse=TRUE)$design
-if(periodicity){
-  Bint = B[,-c(1:p,K+1:p)]
-  Bleft = B[,1:p]
-  Bright = B[,K+1:p]
-  B = cBind(Bint,Bleft+Bright)
+
+bs = "ps"
+
+if(knots.option == "quantile"){
+  bs = "bs"
 }
 
-
-difference.penalty <-function(m,p,K,periodicity=FALSE){
-  
-  # parameter  m: difference order
-  # parameter  p: degree of B-splines
-  # parameter  K: number of interior knots
-  c = rep(0,m+1)
-  
-  for(i in 0:m)
-    c[i+1] = (-1)^(i+1)*factorial(m)/(factorial(i)*factorial(m-i))
-  
-  if(!periodicity){
-  
-    M = matrix(0,nrow=K+p-m,ncol=K+p)
-    for(i in 1:(K+p-m)) M[i,i:(i+m)] = c
-  }
-  if(periodicity){
-    
-    M = matrix(0,nrow=K,ncol=K)
-    for(i in 1:(K-m)) M[i,i:(i+m)] = c
-    for(i in (K-m+1):K) M[i,c(i:K,1:(m-K+i))] = c
-  }
-  
-  return(M)
-}
-
-
-P = difference.penalty(m,p,K,periodicity)
-P1 = Matrix(P)
-P2 = Matrix(t(P))
-P = P2%*%P1
+s.object = s(x=x, bs=bs, k=K+3, sp=NULL)
+object  = smooth.construct(s.object,data = data.frame(x=x),knots=list(x=knots))
+P =  object$S[[1]]
+if(knots.option == "quantile") P = P / max(abs(P))*10 # rescaling
   
 if(is.null(weight)) weight <- rep(1,length(x))
 
@@ -70,8 +45,7 @@ tUPU = Sigi_sqrt%*%(P%*%Sigi_sqrt)
 Esig = eigen(tUPU,symmetric=TRUE)
 U = Esig$vectors
 s = Esig$values
-if(!periodicity) s[(K+p-m+1):(K+p)]=0
-if(periodicity) s[K] = 0
+s[(K+p-m+1):(K+p)]=0
 A = B%*%(Sigi_sqrt%*%U)
 }
 
@@ -86,7 +60,7 @@ List = list(
         "A" = A,
         "B" = B,
         "s" = s,
-        "Sigi.sqrt"=Sigi_sqrt,
+        "Sigi.sqrt" = Sigi_sqrt,
         "U" = U,
         "P" = P)
 
