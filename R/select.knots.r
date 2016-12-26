10510d0c58bbb26179f84db00d5747c0527f8365
cli9 <cli9@ncsu.edu>
2016-12-25 23:57:22 -0500

    Update select.knots.r
    knots placement with slight shift

--- R/select.knots.r
+++ R/select.knots.r
@@ -1,12 +1,23 @@
-
-select.knots <- function(t,knots=10,p=3,option="quantile"){
+select.knots <- function(t,knots=10,p=3,option="equally-spaced"){
   
   qs <- seq(0,1,length=knots+1)
+  max_t <- max(t) 
+  min_t <- min(t) 
+  range_t <- max_t - min_t 
+  
+  min_t <- min_t - range_t * 0.001 # follow Simon Wood's implementation
+  max_t <- max_t + range_t * 0.001 #
   
   if(option=="equally-spaced"){
-    knots <- (max(t)-min(t))*qs + min(t)
+    knots <- (max_t-min_t)*qs + min_t 
   }
   if(option=="quantile"){
+    loc_max <- which.max(t)
+    loc_min <- which.min(t)
+    s <- t+ rnorm(length(t))*range_t/100
+    s[loc_max] <- t[loc_max]
+    s[loc_min] <- t[loc_min]
+    t <- s
     knots <- as.vector(quantile(t,qs))
   }
   
