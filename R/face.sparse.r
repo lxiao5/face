a86f9d11b00ab9c4a7ce565927fac42db4032405
cli9 <cli9@ncsu.edu>
2017-07-10 20:54:12 -0400

    Update face.sparse.r
    change default two_step option to FALSE

--- R/face.sparse.r
+++ R/face.sparse.r
@@ -4,29 +4,41 @@ face.sparse <- function(data, newdata = NULL,
                         p=3,m=2,lambda=NULL,lambda_mean=NULL,
                         search.length=14,
                         lower=-3,upper=10, lower2 = -3, upper2=5,
-                        calculate.scores=FALSE,pve=0.99){
+                        calculate.scores=FALSE,pve=0.99,two_step=FALSE){
 
+  if(!two_step){
+    fit <- face.sparse.inner(data = data, newdata=NULL, W = NULL,
+                             argvals.new = argvals.new,
+                             center=center,
+                             knots=knots,
+                             knots.option = knots.option,
+                             p = p, m = m, lambda= lambda, lambda_mean = lambda_mean,
+                             lower=lower,upper=upper,search.length=search.length,
+                             calculate.scores=calculate.scores, pve = pve)
+    
+  }else{
+    fit <- face.sparse.inner(data = data, newdata=NULL, W = NULL,
+                             argvals.new = argvals.new,
+                             center=center,
+                             knots=knots,
+                             knots.option = knots.option,
+                             p = p, m = m, lambda= lambda, lambda_mean = lambda_mean,
+                             lower=lower,upper=upper,search.length=search.length,
+                             pve = pve)
+    
+    W <- weight.construct(fit,data)
+    
+    
+    fit <- face.sparse.inner(data = data, newdata=newdata, W = W,
+                             argvals.new = argvals.new,
+                             center=center,
+                             knots=knots,
+                             knots.option = knots.option,
+                             p = p, m = m, lambda= lambda, lambda_mean = lambda_mean,
+                             lower=lower2,upper=upper2,search.length=length(lower2:upper2),
+                             calculate.scores=calculate.scores,pve=pve)
+  }
   
-  fit <- face.sparse.inner(data = data, newdata=NULL, W = NULL,
-                           argvals.new = argvals.new,
-                           center=center,
-                           knots=knots,
-                           knots.option = knots.option,
-                           p = p, m = m, lambda= lambda, lambda_mean = lambda_mean,
-                           lower=lower,upper=upper,search.length=search.length,
-                           pve = pve)
-
-  W <- weight.construct(fit,data)
-  
-  
-  fit <- face.sparse.inner(data = data, newdata=newdata, W = W,
-                           argvals.new = argvals.new,
-                           center=center,
-                           knots=knots,
-                           knots.option = knots.option,
-                           p = p, m = m, lambda= lambda, lambda_mean = lambda_mean,
-                           lower=lower2,upper=upper2,search.length=length(lower2:upper2),
-                           calculate.scores=calculate.scores,pve=pve)
 
   return(fit)
 }
